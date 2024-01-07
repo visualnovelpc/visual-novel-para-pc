15111cc3ef56b24edb37b624395b9fce3616c54f
Angel Ramirez <miguelangel8877@outlook.com>
2024-01-06 22:10:19 -0600

    layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config'
-
+import pagefind from 'astro-pagefind'
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), pagefind()],
   site: 'https://eledoce.github.io/visual-novel-para-pc',
   base: '/visual-novel-para-pc/',
 })
