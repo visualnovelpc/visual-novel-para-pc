e0339aafd9366489f1f263d11c01a2053ecc88a6
Angel Ramirez <miguelangel8877@outlook.com>
2024-01-09 00:08:53 -0600

    add pagination

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    format: 'file',
+  },
   integrations: [tailwind(), pagefind()],
   site: 'https://eledoce.github.io/visual-novel-para-pc',
   base: '/visual-novel-para-pc/',
