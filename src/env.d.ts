1d1fd05f6e919a4fcfe10469a1dd38e62b95f106
Angel Ramirez <miguelangel8877@outlook.com>
2024-01-09 20:08:54 -0600

    styles

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+type Entry = {
+  title: string
+  date: string
+  updated: string
+  tags: string[]
+  cover: string
+  urlComments: string
+}
